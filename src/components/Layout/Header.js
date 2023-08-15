a1c3366ce058c1add03e9f0b4bea3dea5d293273
Nataligit config --global user.email natalja_daad@tut.by <natalja_daad@tut.by>
2023-08-15 18:18:28 +0300

    Update HeadercartButton

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,13 +1,14 @@
 import { Fragment } from 'react';
 import mealsImage from '../../assets/meals.jpg';
 import styles from './Header.module.css';
+import HeaderCartButton from './HeaderCartButton';
 
 const Header = (props) => {
     return(
         <Fragment>
             <header className={styles.header}>
                 <h1>Meals</h1>
-                <button>Cart</button>
+                <HeaderCartButton />
             </header>
             <div className={styles['main-image']}>
                 <img src={mealsImage} alt='A table full of delicious food' />
---
+++ src/components/Layout/HeaderCartButton.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import CartIcon from '../Cart/CartIcon';
+import styles from './HeaderCartButton.module.css';
+
+const HeaderCartButton = (props) => {
+    return (
+        <button className={styles.button}>
+            <span className={styles.icon}>
+                <CartIcon />
+            </span>
+            <span>Your Cart</span>
+            <span className={styles.badge}>
+                3
+            </span>
+        </button>
+    );
+};
+
+export default HeaderCartButton;
