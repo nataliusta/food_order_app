1bc7b57c9f40f7099c81f273b4fab0dc1c565092
Nataligit config --global user.email natalja_daad@tut.by <natalja_daad@tut.by>
2023-08-15 16:55:10 +0300

    Update Header.js

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,8 +1,17 @@
-import React from 'react';
+import { Fragment } from 'react';
+import mealsImage from '../../assets/meals.jpg';
 
-const Header = () => {
+const Header = (props) => {
     return(
-
+        <Fragment>
+            <header>
+                <h1>Meals</h1>
+                <button>Cart</button>
+            </header>
+            <div>
+                <img src={mealsImage}></img>
+            </div>
+        </Fragment>
     )
 };
 
