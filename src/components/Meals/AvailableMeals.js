6b927926b3ba52c133971e6b17920cfaa0b821ce
Nataligit config --global user.email natalja_daad@tut.by <natalja_daad@tut.by>
2023-08-15 19:51:58 +0300

    Update AvailabeMeals.js

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,8 +1,42 @@
 import React from 'react';
 
-const AvailableMeals = (props) => {
-    return (
+const DUMMY_MEALS = [
+    {
+      id: 'm1',
+      name: 'Sushi',
+      description: 'Finest fish and veggies',
+      price: 22.99,
+    },
+    {
+      id: 'm2',
+      name: 'Schnitzel',
+      description: 'A german specialty!',
+      price: 16.5,
+    },
+    {
+      id: 'm3',
+      name: 'Barbecue Burger',
+      description: 'American, raw, meaty',
+      price: 12.99,
+    },
+    {
+      id: 'm4',
+      name: 'Green Bowl',
+      description: 'Healthy...and green...',
+      price: 18.99,
+    },
+  ];
 
+const AvailableMeals = (props) => {
+
+    const mealsList = DUMMY_MEALS.map(meal => <li>{meal.name}</li>);
+    return (
+        <section>
+            <ul>
+                {mealsList}
+            </ul>
+
+        </section>
     );
 };
 
