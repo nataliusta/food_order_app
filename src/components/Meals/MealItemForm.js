8de4a4f5388a97e3f2e376faefd370c5a17fab00
Nataligit config --global user.email natalja_daad@tut.by <natalja_daad@tut.by>
2023-08-16 15:07:39 +0300

    Create Input.js

--- src/components/Meals/MealItemForm.js
+++ src/components/Meals/MealItemForm.js
@@ -1,8 +1,20 @@
 import React from 'react';
 
-const MealItemForm = (props) => {
-    return (
+import styles from './MealItemForm.module.css';
+import Input from '../UI/Input';
 
+const MealItemForm = (props) => {
+
+    const onClickHandler = (event) => {
+        event.preventDefault();
+    };
+
+    return (
+        <form className={styles.form}>
+            <label htmlFor="current-savings"></label>
+            <Input />
+            <button onClick={onClickHandler}>+ Add</button>
+      </form>
     )
 };
 
