da69b7e2a371d31db8d7aa1ee23d6c508dfea979
Nataligit config --global user.email natalja_daad@tut.by <natalja_daad@tut.by>
2023-08-16 16:37:10 +0300

    Fixed app

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -32,14 +32,15 @@ const DUMMY_MEALS = [
 
 const AvailableMeals = (props) => {
     const mealsList = DUMMY_MEALS.map((meal) => (
-      <MealItem 
-        key={meal.id} 
-        name={meal.name} 
-        description={meal.description} 
-        price={meal.price} 
+      <MealItem
+          id={meal.id}
+          key={meal.id}
+          name={meal.name}
+          description={meal.description}
+          price={meal.price}
       />
     ));
-    
+
     return (
         <section className={styles.meals}>
           <Card>
--- src/components/Meals/MealItemForm.js
+++ src/components/Meals/MealItemForm.js
@@ -11,14 +11,17 @@ const MealItemForm = (props) => {
 
     return (
         <form className={styles.form}>
-            <Input label='Amount' input={{
-                id: 'amount',
-                type: 'number',
-                min: '1',
-                max: '5',
-                step: '1',
-                defaultValue: '1'
-            }} />
+            <Input
+                label='Amount'
+                input={{
+                    id: 'amount_' + props.id,
+                    type: 'number',
+                    min: '1',
+                    max: '5',
+                    step: '1',
+                    defaultValue: '1',
+                }}
+            />
             <button onClick={onClickHandler}>+ Add</button>
       </form>
     )
