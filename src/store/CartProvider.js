b10ec8cc1688fd6be0c1e67959281685c34ffbd0
Nataligit config --global user.email natalja_daad@tut.by <natalja_daad@tut.by>
2023-08-17 22:26:46 +0300

    Update App.js

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -2,8 +2,19 @@ import React from 'react';
 import CartContext from './cart-context';
 
 const CartProvider = (props) => {
-    return <CartContext.Provider>
+    const addItemToCartHandler = (item) => {};
 
+    const removeItemFromCartHandler = (id) => {};
+
+    const cartContext = {
+        item: [],
+        totalAmount: 0,
+        addItem: addItemToCartHandler,
+        removeItem: removeItemFromCartHandler,
+    };
+
+    return <CartContext.Provider value={cartContext}>
+        {props.children}
     </CartContext.Provider>
 }
 
