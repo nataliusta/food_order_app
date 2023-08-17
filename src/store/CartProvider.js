9a448ac09cb32f5d9f932075c89eb767006686b0
Nataligit config --global user.email natalja_daad@tut.by <natalja_daad@tut.by>
2023-08-17 22:14:31 +0300

    Create CartProvider.js

---
+++ src/store/CartProvider.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import CartContext from './cart-context';
+
+const CartProvider = (props) => {
+    return <CartContext.Provider>
+
+    </CartContext.Provider>
+}
+
+export default CartProvider;
