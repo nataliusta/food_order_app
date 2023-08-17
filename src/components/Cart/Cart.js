2c5eef5d7259c35a59c27931e96315a6ff90423f
Nataligit config --global user.email natalja_daad@tut.by <natalja_daad@tut.by>
2023-08-17 17:14:53 +0300

    Add Modal styles

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import styles from './Cart.module.css';
 
 const Cart = () => {
-    return
+const cartItems = <ul className={styles['cart-items']}>
+        {[{ id: 'c1', name: 'Sushi', amount: 2, price: 12.99}].map((item) => (
+            <li>{item.name}</li>
+        ))}
+        </ul>
+
+    return (
+        <div>
+            {cartItems}
+            <div className={styles.total}>
+                <span>Total Amount</span>
+                <span>35.62</span>
+            </div>
+            <div className={styles.actions}>
+                <button className={styles['button--alt']}>Close</button>
+                <button className={styles.button}>Order</button>
+            </div>
+        </div>
+    )
 }
 
 export default Cart;
