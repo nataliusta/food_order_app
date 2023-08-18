a8f872287cf2ae880fbbcbb525ed11c99a9e9a36
Nataligit config --global user.email natalja_daad@tut.by <natalja_daad@tut.by>
2023-08-18 17:22:01 +0300

    Add useReducer to CartProvider.js

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,14 +1,38 @@
-import React from 'react';
+import React, { useReducer, useRef } from 'react';
 import CartContext from './cart-context';
 
-const CartProvider = (props) => {
-    const addItemToCartHandler = (item) => {};
+const defaultCartState = {
+    items: [],
+    totalAmount: 0,
+};
 
-    const removeItemFromCartHandler = (id) => {};
+const cartReducer = (state, action) => {
+    if (action.type === 'ADD') {
+        const updatedItems = state.items.concat(action.item);
+        const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount;
+        return {
+            items: updatedItems,
+            amount: updatedTotalAmount
+        };
+    }
+    return defaultCartState;
+};
+
+
+const CartProvider = (props) => {
+    const [cartState, dispatchCartAction] = useReducer(cartReducer, defaultCartState);
+
+    const addItemToCartHandler = (item) => {
+        dispatchCartAction({type: 'ADD', item: item});
+    };
+
+    const removeItemFromCartHandler = (id) => {
+        dispatchCartAction({type: 'REMOVE', id: id});
+    };
 
     const cartContext = {
-        item: [],
-        totalAmount: 0,
+        item: cartState.items,
+        totalAmount: cartState.totalAmount,
         addItem: addItemToCartHandler,
         removeItem: removeItemFromCartHandler,
     };
