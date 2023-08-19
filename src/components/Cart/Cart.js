b36cb37bea8abd1bdf269878452852c149a5ee49
Nataligit config --global user.email natalja_daad@tut.by <natalja_daad@tut.by>
2023-08-19 18:06:09 +0300

    Update Cart.js

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,5 +1,7 @@
 import React, { useContext } from 'react';
+
 import Modal from '../UI/Modal';
+import CartItem from './CartItem';
 import styles from './Cart.module.css';
 import CartContext from '../../store/cart-context';
 
@@ -9,12 +11,23 @@ const Cart = (props) => {
     const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
     const hasItems = cartCtx.items.length > 0;
 
+    const cartItemRemoveHandler = id => {};
+
+    const cartItemAddHandler = item => {};
+
     const cartItems = (
         <ul className={styles['cart-items']}>
             {cartCtx.items.map((item) => (
-                <li>{item.name}</li>
+                <CartItem 
+                    key={item.id} 
+                    name={item.name} 
+                    amount={item.amount} 
+                    price={item.price}
+                    onRemove={cartItemRemoveHandler.bind(null, item.id)}
+                    onAdd={cartItemAddHandler.bind(null, item)}
+                />
             ))}
-            </ul>
+        </ul>
     );
 
     return (
