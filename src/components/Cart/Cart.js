ddda1848f61dc95de1a1330eb0652f8290671bc4
Nataligit config --global user.email natalja_daad@tut.by <natalja_daad@tut.by>
2023-08-19 17:21:10 +0300

    Upload Cart.js

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Modal from '../UI/Modal';
 import styles from './Cart.module.css';
+import CartContext from '../../store/cart-context';
 
 const Cart = (props) => {
+    const cartCtx = useContext(CartContext);
+
+    const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+    const hasItems = cartCtx.items.length > 0;
+
 const cartItems = <ul className={styles['cart-items']}>
-        {[{ id: 'c1', name: 'Sushi', amount: 2, price: 12.99}].map((item) => (
+        {cartCtx.items.map((item) => (
             <li>{item.name}</li>
         ))}
         </ul>
@@ -14,11 +20,11 @@ const cartItems = <ul className={styles['cart-items']}>
             {cartItems}
             <div className={styles.total}>
                 <span>Total Amount</span>
-                <span>35.62</span>
+                <span>{totalAmount}</span>
             </div>
             <div className={styles.actions}>
                 <button className={styles['button--alt']} onClick={props.onClose}>Close</button>
-                <button className={styles.button}>Order</button>
+                {hasItems && <button className={styles.button}>Order</button>}
             </div>
         </Modal>
     )
