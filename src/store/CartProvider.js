3114aa88050d33ed97aa2a6aa1402ac5e8d640d6
Nataligit config --global user.email natalja_daad@tut.by <natalja_daad@tut.by>
2023-08-22 18:09:26 +0300

    Update CartProvider

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -8,8 +8,25 @@ const defaultCartState = {
 
 const cartReducer = (state, action) => {
     if (action.type === 'ADD') {
-        const updatedItems = state.items.concat(action.item);
         const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount;
+
+        const existingCartItemIndex = state.items.findIndex(
+            (item) => item.id === action.item.id
+        );
+        const existingCartItem = state.items[existingCartItemIndex];
+        let updatedItems;
+
+        if (existingCartItem) {
+            const updatedItem = {
+                ...existingCartItem,
+                amount: existingCartItem.amount + action.item.amout
+            };
+            updatedItems = [...state.items];
+            updatedItems[existingCartItemIndex] = updatedItem;
+        } else {
+            updatedItems = state.items.concat(action.item);
+        }
+
         return {
             items: updatedItems,
             amount: updatedTotalAmount
