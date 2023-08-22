f984c645c7abdca340fe96c4921e22b8abd8bc52
Nataligit config --global user.email natalja_daad@tut.by <natalja_daad@tut.by>
2023-08-22 18:34:02 +0300

    Update Cartprovider

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,4 +1,4 @@
-import React, { useReducer, useRef } from 'react';
+import React, { useReducer } from 'react';
 import CartContext from './cart-context';
 
 const defaultCartState = {
@@ -13,6 +13,7 @@ const cartReducer = (state, action) => {
         const existingCartItemIndex = state.items.findIndex(
             (item) => item.id === action.item.id
         );
+
         const existingCartItem = state.items[existingCartItemIndex];
         let updatedItems;
 
@@ -30,6 +31,26 @@ const cartReducer = (state, action) => {
         return {
             items: updatedItems,
             amount: updatedTotalAmount
+        };
+    }
+    if (action.type === 'REMOVE') {
+        const existingCartItemIndex = state.items.findIndex(
+            (item) => item.id === action.item.id
+        );
+        const existingItem = state.items[existingCartItemIndex];
+        const updatedTotalAmount = state.totalAmount - existingItem.price;
+        let updatedItems;
+
+        if (existingItem.amount === 1) {
+            updatedItems = state.items.filter(item => item.id !== action.id);
+        } else {
+            const updateItem = { ...existingItem, amount: existingItem.amount - 1 };
+            updatedItems = [...state.items];
+            updatedItems[existingCartItemIndex] = updateItem;
+        }
+        return {
+            items: updatedItems,
+            totalAmount: updatedTotalAmount
         };
     }
     return defaultCartState;
