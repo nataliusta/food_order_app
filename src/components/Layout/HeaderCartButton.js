e7bca21c8502a9cd115f1b624383c063dcc15cec
Nataligit config --global user.email natalja_daad@tut.by <natalja_daad@tut.by>
2023-08-22 22:14:27 +0300

    Add useEffect to HeaderCartButton

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,24 +1,42 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import CartIcon from '../Cart/CartIcon';
 import CartContext from '../../store/cart-context';
 import styles from './HeaderCartButton.module.css';
 
 const HeaderCartButton = (props) => {
+    const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
     const cartCtx = useContext(CartContext);
 
-    const numberOfCartItems = cartCtx.items.reduce((currentNum, item) => {
+    const { items } = cartCtx;
+
+    const numberOfCartItems = items.reduce((currentNum, item) => {
         return currentNum + item.amount;
     }, 0);
 
+    const btnClasses = `${styles.button} ${btnIsHighlighted ? styles.bump : ''}`;
+
+    useEffect(() => {
+        if (items.length === 0) {
+            return;
+        }
+        setBtnIsHighlighted(true);
+
+        const timer = setTimeout(() => {
+            setBtnIsHighlighted(false);
+        }, 300);
+
+        return () => {
+            clearTimeout(timer);
+        };
+    }, [items]);
+
     return (
-        <button className={styles.button} onClick={props.onClick}>
+        <button className={btnClasses} onClick={props.onClick}>
             <span className={styles.icon}>
                 <CartIcon />
             </span>
             <span>Your Cart</span>
-            <span className={styles.badge}>
-                {numberOfCartItems}
-            </span>
+            <span className={styles.badge}>{numberOfCartItems}</span>
         </button>
     );
 };
