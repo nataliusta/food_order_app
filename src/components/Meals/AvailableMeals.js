e316251bf31a0fe1999a22425cbaa30985696f60
Nataligit config --global user.email natalja_daad@tut.by <natalja_daad@tut.by>
2023-08-29 12:55:46 +0300

    Add GET request

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+
 import Card from '../UI/Card';
 import MealItem from './MealItem';
 import styles from './AvailableMeals.module.css';
 
 const AvailableMeals = () => {
+    useEffect(() => {
+      const fetchMeals = async () => {
+          const response = await fetch('https://console.firebase.google.com/project/food-react-e2f74/database/food-react-e2f74-default-rtdb/data/~2F/Meals.json')
+        .then();
+        const responseData = await response.json();
+
+        const loadedMeals = [];
+
+        for (const key in responseData) {
+          loadedMeals.push({
+            id: key,
+            name: responseData[key].name,
+            description: responseData[key].description,
+            price: responseData[key].price,
+          });
+          
+        }
+      };
+
+      fetchMeals();
+    }, []);
+
     const mealsList = DUMMY_MEALS.map((meal) => (
       <MealItem
           key={meal.id}
