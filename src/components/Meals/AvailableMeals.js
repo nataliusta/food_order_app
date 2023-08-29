6b52667715bc054e5a0a4b6af65da206130453a9
Nataligit config --global user.email natalja_daad@tut.by <natalja_daad@tut.by>
2023-08-29 13:41:27 +0300

    Add loading state

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -6,6 +6,7 @@ import styles from './AvailableMeals.module.css';
 
 const AvailableMeals = () => {
   const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
       const fetchMeals = async () => {
@@ -24,10 +25,19 @@ const AvailableMeals = () => {
           });
         }
         setMeals(loadedMeals);
+        setIsLoading(false);
       };
 
       fetchMeals();
     }, []);
+
+    if (isLoading) {
+      return (
+        <section className={styles.MealsLoading}>
+          <p>Loading...</p>
+        </section>
+      );
+    }
 
     const mealsList = meals.map((meal) => (
       <MealItem
